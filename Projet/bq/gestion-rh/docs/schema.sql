7ef1374aa71cb18254ee7594d8fa9378ef163ce8
Ismael <ismael@mg-consulting-it-act.com>
2026-06-02 13:57:24 +0300

    creation postes pour gestion rh

--- Projet/bq/gestion-rh/docs/schema.sql
+++ Projet/bq/gestion-rh/docs/schema.sql
@@ -11,3 +11,14 @@ CREATE TABLE departments (
     department_id INTEGER PRIMARY KEY AUTOINCREMENT,
     department_name TEXT NOT NULL UNIQUE
 );
+
+CREATE TABLE positions (
+    position_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    position_name TEXT NOT NULL,
+    base_salary DECIMAL(12,2) NOT NULL
+);
+
+INSERT INTO positions
+(position_name, base_salary)
+VALUES
+('Développeur COBOL',1500000);
