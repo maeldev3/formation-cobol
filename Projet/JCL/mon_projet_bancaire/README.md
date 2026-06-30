2a60f54161f8d585829d7cb7f19b9a06bf5d592f
Ismael <ismael@mg-consulting-it-act.com>
2026-06-30 16:27:26 +0300

    supprimer pr bancaire

--- Projet/JCL/mon_projet_bancaire/README.md
+++
@@ -1,2 +0,0 @@
-# Projet bancaire COBOL
-Consignes dans le README.
--- Projet/JCL/mon_projet_bancaire/scripts/submit_job.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]; then echo "Usage: $0 fichier.jcl"; exit 1; fi
-zowe zos-jobs submit local-file "$1" --host 204.90.115.200 --port 10443 --user Z75013 --password XAW74XAZ --reject-unauthorized false --wait-for-output --view-all-spool-content
