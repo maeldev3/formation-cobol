29e31245f3160cdbd5b94d41866d7342912686e9
Ismael <ismael@mg-consulting-it-act.com>
2026-07-03 08:49:28 +0300

    tp1,2,3,2

---
+++ Projet/Z74830/TP1-HELLO/HELLO1.cbl
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO1.
+
+       PROCEDURE DIVISION.
+           DISPLAY "********************************".
+           DISPLAY "*      BIENVENUE SUR IBM Z      *".
+           DISPLAY "*        HELLO WORLD !          *".
+           DISPLAY "********************************".
+           STOP RUN.
